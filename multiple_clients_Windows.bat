974f5a3eeacfd97676cfe23a45ef4e2db3f788a2
New Age Soldier <newgmobile@gmail.com>
2020-03-12 09:14:12 -0400

    Update multiple_clients_Windows.bat

--- multiple_clients_Windows.bat
+++ multiple_clients_Windows.bat
@@ -1,1 +1,4 @@
-START C:\ProgramData\GameMakerStudio2\Cache\runtimes\runtime-2.2.5.378\windows\Runner.exe -game %LocalAppData%\GameMakerStudio2\GMS2TEMP\mmo_AEA4461_VM\mmo.win
+REM This is for running multiple game clients to test server code. If you run the game from GMS2, it will compile a new .win file (game client).
+REM Replace (REPLACE WITH LATEST COMPILE) with the highest number or latest version. Browse those folders to find it.
+
+START C:\ProgramData\GameMakerStudio2\Cache\runtimes\(REPLACE WITH LATEST COMPILE)\windows\Runner.exe -game %LocalAppData%\GameMakerStudio2\GMS2TEMP\(REPLACE WITH LATEST COMPILE)\mmo.win
